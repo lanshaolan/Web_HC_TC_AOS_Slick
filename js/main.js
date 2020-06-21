0b4c98329a57771ca77d56df361bcc57d6f1dc7d
KID421 <suchild7@gmail.com>
2020-06-21 16:32:39 +0800

    調整

--- js/main.js
+++ js/main.js
@@ -1,20 +1,17 @@
-$(document).ready(function () {
-    AOS.init();
+AOS.init();
 
-    $('#banner').slick({
-        autoplay: true,
-        autoplaySpeed: 1000,
-        arrows: false
-    });
-
-    $('#brand').slick({
-        slidesToShow: 5,
-        slidesToScroll: 2,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        dots: true,
-        arrows: false,
-        variableWidth: true,
-    });
+$('#banner').slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false
 });
 
+$('#brand').slick({
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true,
+    arrows: false,
+    variableWidth: true,
+});
