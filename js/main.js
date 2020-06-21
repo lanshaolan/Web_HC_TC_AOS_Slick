0a928473010039336cd8891e2795e6c2edf91f90
KID421 <suchild7@gmail.com>
2020-06-21 15:42:24 +0800

    輪播圖樣式與設定

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
-$('#banner').slick();
-$('#brand').slick();
+$('#banner').slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false
+});
+
+$('#brand').slick({
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true,
+    arrows: false,
+    variableWidth: true
+});
